168905a79cca485cd6503fb87367d6777aafe069
hoeppken <rafael_rsv@hotmail.com>
2025-01-13 22:23:03 +0100

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Rafael dos Santos Varela on 09.01.25.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
---
+++ Menu/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Rafael dos Santos Varela on 13.01.25.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name: String
+    var price: String
+    var imageName: String
+}
